749b0d03c95dbbd4bb0feaeee2927644cd5c6a57
willerce <willerce@gmail.com>
2015-05-05 15:06:37 +0800

    添加 Express 中间件实现实时编译

---
+++ server.js
@@ -0,0 +1,21 @@
+var serveComponent = require('component-middleware');
+var serveStatic = require('serve-static');
+var express = require('express');
+var app = express();
+
+app.use(serveComponent());
+app.use(serveStatic(__dirname));
+app.use(serveStatic(__dirname + '/lib'));
+
+app.get('/', function (req, res) {
+    res.send('Hello WxMoment!');
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
